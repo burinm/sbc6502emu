1600fa0378275c9adf112260f265f9441f3e8fa0
burin <Micheal.Burin@colorado.edu>
2016-12-03 22:17:32 -0700

    Comments for sbc_emu

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/* SBC emulator
+
+2015 - Micheal Burin - base loop, graphics and SDL routines
+2016 - Micheal Burin - extended for 5613, adding sbc components,
+	and replacing gui with blinking LEDs 
+
+	Uses M6502 core by Marat Fayzullin and Alex Krasivsky
+
+*/
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
@@ -22,26 +31,21 @@
 #include "6522.h"
 
 typedef struct {
-                    uint8_t* memory;
-                    uint16_t size;
-                    uint16_t start;
-                    char*   desc;
-                } memory_6502_t;
+	uint8_t* memory;
+	uint16_t size;
+	uint16_t start;
+	char*   desc;
+} memory_6502_t;
 
 // Green LEDs
 uint8_t green_led_array_porta;
 uint8_t green_led_array_portb;
 
-
-
 #define MEM_SIZE    (64 * 1024)
-
 #define SRAM_SIZE   (32 * 1024)
 #define SRAM_START  0x0000
-
 #define ROM_SIZE    ( 8 * 1024)
 #define ROM_START   0xe000
-
 #define RTS         0x60 //Return from Subroutine
 
 // Not sure how to emulate without an SPI engine...
@@ -51,18 +55,15 @@ uint8_t green_led_array_portb;
 
 
 // Emulation memory mapped devices
+uint8_t*  memory;
 static memory_6502_t memory_sram;
 static memory_6502_t memory_rom;
 static memory_6502_t via0_ram;
 
-uint8_t*  memory;
 pthread_mutex_t mem_lock=PTHREAD_MUTEX_INITIALIZER;
-//pthread_mutex_t mem_lock=PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
 #define MEM_LOCK    { pthread_mutex_lock(&mem_lock); }
 #define MEM_UNLOCK  { pthread_mutex_unlock(&mem_lock); }
 
-
-//TODO mutexes
 uint8_t sbc_status;
 pthread_mutex_t sbc_status_lock=PTHREAD_MUTEX_INITIALIZER;
 #define SBC_STATUS_LOCK    { pthread_mutex_lock(&sbc_status_lock); }
@@ -70,7 +71,6 @@ pthread_mutex_t sbc_status_lock=PTHREAD_MUTEX_INITIALIZER;
 
 //Threads
 void *runCPU(void *p);
-void *runVIA0(void *p);
 
 void printM6502(M6502 *R);
 void load_rom(char* filename, uint16_t address, memory_6502_t* memory);
@@ -79,9 +79,7 @@ void memory_dump(memory_6502_t *);
 //sbc state
 #define SBC_RUNNING     0x1
 
-
-
-//Escape!!
+// Setup signal to recognize ctrl-C
 void ctrlC(int sig)
 {
     signal(sig, SIG_IGN);
@@ -93,28 +91,23 @@ void ctrlC(int sig)
 
 int main() {
 signal(SIGINT, ctrlC);
-
-//pthread_mutex_init(&mem_lock,NULL);
-//pthread_mutex_init(&sbc_status_lock,NULL);
-
 memory=(uint8_t*)calloc(MEM_SIZE,1);
 
-
-//Initialize ram, fill with RTS so errand code will
-// jump to handler
+// Initialize ram, fill with RTS so errant code will
+//  jump to handler
 memory_sram.memory=memory;
 memset(memory_sram.memory,RTS,(SRAM_SIZE));
 memory_sram.size=SRAM_SIZE;
 memory_sram.start=SRAM_START;
 memory_sram.desc="32K-main-ram";
 
-//Initialize rom
+// Initialize rom
 memory_rom.memory=memory;
 memory_rom.size=ROM_SIZE;
 memory_rom.start=ROM_START;
 memory_rom.desc="8K-main-rom";
 
-//Initialize VIAs
+// Initialize VIAs
 via0_ram.memory=memory;
 via0_ram.size=DEVICE_6522_NUM_REG;
 via0_ram.start=VIA_0_START;
@@ -124,11 +117,12 @@ device_6522_init((uint8_t*)(via0_ram.memory + via0_ram.start));
 //Initialize FRAM
 //fram=(uint8_t*)calloc(FRAM_SIZE,1);
 
-//Load rom
+// Load rom
+// TODO: make this settable from command line
 //load_rom("sbc.rom", 0xe000, &memory_rom);
 load_rom("rom.bin", 0x0200, &memory_rom);
 
-//Set machine to running
+// Set machine to running
 sbc_status=SBC_RUNNING;
 
 // x2 8 Green LEDs
@@ -137,21 +131,6 @@ green_led_array_porta=0; // all off
 
 //Start console
 console_main();
-
-// Maybee use SDL thread....?
-//SDL_Thread *thread;
-//    int thread_id=0;
-//if ((thread=SDL_CreateThread(runCPU, "CPUThread", (void *)NULL)) == NULL) { printf("couldn't creat CPU thread\n"); goto error;}
-
-
-#if 0
-pthread_t via0_thread;
-memset((void*)&via0_thread,0,sizeof(pthread_t));
-
-int via0_handle;
-via0_handle = pthread_create(&via0_thread,NULL,runVIA0,NULL);
-printf("Started VIA0\n");
-#endif
 
 pthread_t cpu_thread;
 memset((void*)&cpu_thread,0,sizeof(pthread_t));
@@ -165,14 +144,9 @@ while (sbc_status & SBC_RUNNING) {
 }
 
 pthread_join(cpu_thread,NULL);
-//pthread_join(via0_thread,NULL);
-
-//pthread_mutex_destroy(&mem_lock);
+pthread_mutex_destroy(&mem_lock);
 
 console_quit();
-
-
-
 } 
 
 void *runCPU(void *p)
@@ -194,23 +168,6 @@ printf("runCPU exit\n");
 return 0;
 }
 
-void *runVIA0(void *p) { 
-
-while (sbc_status & SBC_RUNNING) {
-    //MEM_LOCK
-    // Set LEDs to match port B output
-    //green_led_array_portb = *(uint8_t*)(via0_ram.memory + via0_ram.start + DEVICE_6522_REG_RB);
-    //printf("b - %x ", *(uint8_t*)(via0_ram.memory + via0_ram.start + DEVICE_6522_REG_RB));
-
-    // Set LEDs to match port A output
-    //green_led_array_porta = *(uint8_t*)(via0_ram.memory + via0_ram.start + DEVICE_6522_REG_RA);
-    //printf("a - %x ", *(uint8_t*)(via0_ram.memory + via0_ram.start + DEVICE_6522_REG_RA));
-    //MEM_UNLOCK
-}
-printf("runVIA0 exit\n");
-
-}
-
 void printM6502(M6502 *R)
 {
     printf("A=%.2x P=%.2x X=%.2x Y=%.2x S=%.2x  PC=%.2x%.2x\n",R->A, R->P, R->X, R->Y, R->S, R->PC.B.h, R->PC.B.l);
@@ -221,7 +178,6 @@ void printM6502(M6502 *R)
 
 void Wr6502(register word Addr,register byte Value)
 {   
-//MEM_LOCK
     if (Addr >= memory_sram.start && Addr < memory_sram.start + memory_sram.size ) { 
         *(uint8_t *)(memory_sram.memory + Addr) = Value;
         //printf("write ram %04x:%02x\n",Addr,*(uint8_t *)(memory_sram.memory + Addr));
@@ -240,12 +196,10 @@ void Wr6502(register word Addr,register byte Value)
     if (Addr >= memory_rom.start && Addr < memory_rom.start + memory_rom.size ) { 
         printf("write rom !! %04x:%02x error\n",Addr,*(uint8_t *)(memory_rom.memory + Addr));
     }
-//MEM_UNLOCK
 }
 
 byte Rd6502(register word Addr)
 {
-//MEM_LOCK
     if (Addr >= memory_sram.start && Addr < memory_sram.start + memory_sram.size ) { 
         //printf("read ram %04x:%02x\n",Addr,*(uint8_t *)(memory_sram.memory + Addr));
         return *(uint8_t *)(memory_sram.memory + Addr);
@@ -260,7 +214,6 @@ byte Rd6502(register word Addr)
         //printf("read rom %04x:%02x\n",Addr,*(uint8_t *)(memory_rom.memory + Addr));
         return *(uint8_t *)(memory_rom.memory + Addr);
     }
-//MEM_UNLOCK
 }
 
 /* Unimpelemented. Used for operations that don't need to read memory
@@ -269,15 +222,14 @@ byte Rd6502(register word Addr)
  *      byte Op6502(register word Addr);
  */
 
+//Input/Interrupts 
 byte Loop6502(register M6502 *R)
 {
 uint32_t i;
 
-    //Input/Interrupts 
-
     //nanosleep(&t,NULL);
     //usleep(1);
-    for (i=0;i<300;i++);
+    for (i=0;i<150;i++);
     
     if (! (sbc_status & SBC_RUNNING) ) return (INT_QUIT);
     return (INT_NONE);
@@ -288,7 +240,7 @@ byte Patch6502(register byte Op,register M6502 *R)
     printf ("unknown opcode 0x%x\n", Op);
     printM6502(R);
     printf ("------\n");
-    //assert(0);
+    assert(0);
     return 1;
 }
 
@@ -300,7 +252,7 @@ FILE* f;
 struct stat stat_s;
 off_t rom_size;
 
-    //open rom
+    // Open rom
     f = fopen(filename,"r");
 
     if (f == NULL) {
@@ -347,4 +299,3 @@ void memory_dump(memory_6502_t *memory) {
     }
     printf("\n");
 }
-
